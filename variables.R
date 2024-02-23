ac324f1f73e4843d8498bdadfa58dd0b9969a65b
trikoh94 <trikoh94@gmail.com>
2024-02-23 21:44:23 +0900

    updates for Rstudion

---
+++ variables.R
@@ -0,0 +1,27 @@
+A <-10
+B <-5
+
+C <-A+B
+C
+
+
+#variable1
+var1 <-2.5
+#variable2
+var2 <-4   #carriage return 
+
+result<-var1/var2
+result
+
+result<-var1-var2
+result
+
+answer <-sqrt(var2)
+answer
+
+greeting <- "Hello"  #character
+name<-"Bob"
+message<- paste(greeting,name)
+message
+
+
